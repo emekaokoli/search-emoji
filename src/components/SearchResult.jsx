81fe76da407369ceb6f1195ca6a7593d94d18003
Emeka Okoli <emekaokoli00@gmail.com>
2021-12-11 00:02:16 +0100

    pull updates

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ListGroup, Card } from 'react-bootstrap';
 
-const SearchResults = ({ data }) => {
-  return (
-    <Card style={{ width: '18rem', margin: '0 auto' }}>
-      <ListGroup variant='flush' className='d-flex'>
-        {data?.map((emoji, index) => (
-          <ListGroup.Item key={index}>{emoji?.symbol}</ListGroup.Item>
-        ))}
-      </ListGroup>
-    </Card>
-  );
-};
+const SearchResults = ({ data }) => (
+  <Card style={{ width: '18rem', margin: '0 auto' }}>
+    <ListGroup variant='flush' className='d-flex'>
+      {data?.map((emoji, index) => (
+        <ListGroup.Item key={index}>{emoji?.symbol}</ListGroup.Item>
+      ))}
+    </ListGroup>
+  </Card>
+);
 export default SearchResults;
 
 SearchResults.propTypes = {
